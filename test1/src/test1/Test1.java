53af58eef5b5c8b749c557df627dcbf0718a8a43
ansontso <709049662@qq.com>
2020-02-07 16:34:48 +0800

    1

--- test1/src/test1/Test1.java
+++ test1/src/test1/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		int i=5;
 		System.out.println(i);
+		System.out.println(6);
 	}
 }
---
+++ test1/src/test1/Test3.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Test3 {
+	public static void main(String[] args) {
+		int i=5;
+		System.out.println(i);
+		System.out.println(2);
+		System.out.println(1);
+	}
+}
+
